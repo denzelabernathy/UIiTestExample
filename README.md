70b7b3753ce571a927193f246edb6c8671bf45c4
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-08-23 10:58:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An refernce of how I utilize Cypress for Web UI test automation!
---
+++ cypress/integration/prodkroger.spec.js
@@ -0,0 +1,5 @@
+/// <reference types ="cypress" />
+
+it('should navigate to the Kroger Prod Web App', () =>{
+    cy.visit('https://www.kroger.com/')
+})
