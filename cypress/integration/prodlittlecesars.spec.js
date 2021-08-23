e498849139be05bf4718f0dd537c63d3fcf1357f
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-08-23 11:48:41 -0400

    added second test

--- cypress/integration/prodlittlecesars.spec.js
+++ cypress/integration/prodlittlecesars.spec.js
@@ -1,5 +1,10 @@
 /// <reference types ="cypress" />
 
-it('should navigate to the Little Caesars Prod Web App', () =>{
-    cy.visit('https://littlecaesars.com/en-us/')
+it('should navigate to the Little Caesars Prod Web App', () => {
+    cy.visit('https://littlecaesars.com/')
+})
+
+it('should be able to navigate to delivery page', () => {
+    cy.visit('https://littlecaesars.com/')
+    cy.get('[data-testid=homePage__delivery-link]').click()
 })
