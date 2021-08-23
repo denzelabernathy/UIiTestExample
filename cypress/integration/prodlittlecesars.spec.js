a2b0ff68981a34f121f0412ca89ad3711bc2867b
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-08-23 14:19:41 -0400

    added more tests to delivery suite

--- cypress/integration/prodlittlecesars.spec.js
+++ cypress/integration/prodlittlecesars.spec.js
@@ -1,7 +1,5 @@
 /// <reference types ="cypress" />
 
-
-
 it('should navigate to the Little Caesars Prod Web App', () => {
     cy.visit('https://littlecaesars.com/')
     cy.get('[data-testid=homePage__delivery-link]').should('have.attr', 'href')
@@ -14,33 +12,29 @@ it('should be able to navigate to delivery page', () => {
     cy.get('[data-testid=locator__delivery-link]').should('be.visible')
 })
 
-// it('has ability to enter delivery address', () => {
-//     cy.visit('https://littlecaesars.com/')
-//     cy.get('[data-testid=homePage__delivery-link]').click()
-//     cy.get('[data-testid=locator__deliveryAddr-field]').click()
-//     cy.get('[data-testid=locator__deliveryAddr-field]').type('111 Monument Cir, Indianapolis, IN 46204')
-//     //cy.get('#\30 typeAheadPrediction').click()
-// })
+it('has ability to select delivery address', () => {
+    cy.visit('https://littlecaesars.com/')
+    cy.get('[data-testid=homePage__delivery-link]').click()
+    cy.get('[data-testid=locator__deliveryAddr-field]').click()
+    cy.get('[data-testid=locator__deliveryAddr-field]').type('111 Monument Cir, Indianapolis, IN 46204')
+    cy.get('[data-testid=delivery__addrPrediction]').click()
+    cy.get('[data-testid=locator__delivery-start-your-order]').should('be.visible')
+    cy.get('[data-testid=locator__delivery-start-your-order]').click()
+})
 
-// it('has ability to select delivery address', () => {
-//     cy.visit('https://littlecaesars.com/')
-//     cy.get('[data-testid=homePage__delivery-link]').click()
-//     cy.get('[data-testid=locator__deliveryAddr-field]').click()
-//     cy.get('[data-testid=locator__deliveryAddr-field]').type('111 Monument Cir, Indianapolis, IN 46204')
-//     cy.get('[data-testid=delivery__addrPrediction]').click()
-// })
-
-// it('should be able to navigate to pickup page', () => {
-//     cy.visit('https://littlecaesars.com/')
-//     cy.get('[data-testid=homePage__pickup-link]').click()
-//     cy.get('[data-testid=locator__pickup]').click()
-//     cy.get('[data-testid=locator__pickup]').type('111 Monument Cir, Indianapolis, IN 46204')
-// })
-
-// it('should be able to find a location nearest to your address', () => {
-//     cy.visit('https://littlecaesars.com/')
-//     cy.get('[data-testid=homePage__pickup-link]').click()
-//     cy.get('[data-testid=locator__pickup]').click()
-//     cy.get('[data-testid=locator__pickup]').type('111 Monument Cir, Indianapolis, IN 46204')
-//     cy.get('[data-testid=locator__pickup-find-store]').click()
-// })
+it('has ability to start order for delivery for custom pizza with bacon', () => {
+    cy.visit('https://littlecaesars.com/')
+    cy.get('[data-testid=homePage__delivery-link]').click()
+    cy.get('[data-testid=locator__deliveryAddr-field]').click()
+    cy.get('[data-testid=locator__deliveryAddr-field]').type('111 Monument Cir, Indianapolis, IN 46204')
+    cy.get('[data-testid=delivery__addrPrediction]').click()
+    cy.get('[data-testid=locator__delivery-start-your-order]').click()
+    cy.get('[data-testid=menu__create-your-own-pizza]').should('be.visible')
+    cy.get('[data-testid=menu__create-your-own-pizza]').click()
+    cy.get('.sc-fATqzn > .sc-hgzKov > .sc-jhAzac > #select')
+        .should('be.visible')
+        .and('have.text', 'RoundThin CrustStuffed Crust RoundDEEP!DEEP!™ Dish')
+    cy.get(':nth-child(5) > .cqTsbT > .dYBQNA > :nth-child(1) > .sc-EHOje > .sc-bdVaJa').click()
+    cy.get(':nth-child(5) > .cqTsbT > .dYBQNA > :nth-child(1) > .sc-EHOje > .sc-bdVaJa')
+        .should('not.be.enabled')
+})
